22b937aba32665260b4c63688ea94b136ae81e37
Boyinf <Gustavoboy1702@gmail.com>
2024-03-15 10:32:35 -0300

    Version 0.1.8

--- shscan/main.py
+++ shscan/main.py
@@ -11,22 +11,21 @@ def get_security_headers(url, ssl=False, cookies=None):
             response = requests.get(url, allow_redirects=True, timeout=5, verify=False, cookies=cookies)
         security_headers = {
              'Content-Security-Policy': response.headers.get('Content-Security-Policy'),
-             'X-Content-Type-Options': response.headers.get('X-Content-Type-Options'),
-             'X-Frame-Options': response.headers.get('X-Frame-Options'),
-             'X-XSS-Protection': response.headers.get('X-XSS-Protection'),
              'Strict-Transport-Security': response.headers.get('Strict-Transport-Security'),
              'Referrer-Policy': response.headers.get('Referrer-Policy'),
              'Feature-Policy': response.headers.get('Feature-Policy'),
+             'Permissions-Policy': response.headers.get('Permissions-Policy'),
+             'X-Content-Type-Options': response.headers.get('X-Content-Type-Options'),
+             'X-Frame-Options': response.headers.get('X-Frame-Options'),
+             'X-XSS-Protection': response.headers.get('X-XSS-Protection'),
              'X-Download-Options': response.headers.get('X-Download-Options'),
              'X-Content-Security-Policy': response.headers.get('X-Content-Security-Policy'),
-             'X-Webkit-CSP': response.headers.get('X-Webkit-CSP'),
              'Content-Security-Policy-Report-Only': response.headers.get('Content-Security-Policy-Report-Only'),
              'Clear-Site-Data': response.headers.get('Clear-Site-Data'),
              'Cross-Origin-Embedder-Policy': response.headers.get('Cross-Origin-Embedder-Policy'),
              'Cross-Origin-Opener-Policy': response.headers.get('Cross-Origin-Opener-Policy'),
              'Cross-Origin-Resource-Policy': response.headers.get('Cross-Origin-Resource-Policy'),
-             'Feature-Policy': response.headers.get('Feature-Policy'),
-             'Permissions-Policy': response.headers.get('Permissions-Policy'),
+             'X-Webkit-CSP': response.headers.get('X-Webkit-CSP'),
 
         }
         return security_headers
